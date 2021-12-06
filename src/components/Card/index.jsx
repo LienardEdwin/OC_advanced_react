168844ca84ec8be676d99a7c91271a6a44df97fe
Edwin Lienard <lienardedwin@gmail.com>
2021-12-06 11:04:24 +0100

    Exo 08: Testez vos composants avec React Testing Library

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
-import DefaultPicture from '../../assets/profile.png'
 import { useTheme } from '../../utils/hooks'
+import DefaultPicture from '../../assets/profile.png'
 
 const CardLabel = styled.span`
   color: ${({ theme }) => (theme === 'light' ? colors.primary : '#ffffff')};
@@ -11,11 +12,14 @@ const CardLabel = styled.span`
   padding-left: 15px;
 `
 
-const CardTitle = styled.span`
+const CardTitle = styled.div`
   color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   font-size: 22px;
   font-weight: normal;
   align-self: center;
+  height: 25px;
+  display: flex;
+  align-items: center;
 `
 
 const CardImage = styled.img`
@@ -31,11 +35,10 @@ const CardWrapper = styled.div`
   justify-content: space-around;
   padding: 15px;
   background-color: ${({ theme }) =>
-    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+          theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
   border-radius: 30px;
   width: 300px;
   height: 300px;
-
   &:hover {
     cursor: pointer;
   }
@@ -43,12 +46,16 @@ const CardWrapper = styled.div`
 
 function Card({ label, title, picture }) {
   const { theme } = useTheme()
+  const [isFavorite, setIsFavorite] = useState(false)
+  const star = isFavorite ? '⭐️' : ''
 
   return (
-    <CardWrapper theme={theme}>
+    <CardWrapper theme={theme} onClick={() => setIsFavorite(!isFavorite)}>
       <CardLabel theme={theme}>{label}</CardLabel>
       <CardImage src={picture} alt="freelance" />
-      <CardTitle theme={theme}>{title}</CardTitle>
+      <CardTitle theme={theme}>
+        {star} {title} {star}
+      </CardTitle>
     </CardWrapper>
   )
 }
@@ -60,8 +67,8 @@ Card.propTypes = {
 }
 
 Card.defaultProps = {
-  title: '',
   label: '',
+  title: '',
   picture: DefaultPicture,
 }
 
